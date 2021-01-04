81c98c7155d277c0fd74f9046a17fff37886d1ba
Craig Gosnay <craig.gosnay@gmail.com>
2021-01-03 21:35:58 -0500

    processes

--- thumbnail_maker.py
+++ thumbnail_maker.py
@@ -8,6 +8,7 @@ from queue import Queue
 from threading import Thread
 import PIL
 from PIL import Image
+import multiprocessing
 
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
@@ -15,99 +16,63 @@ THREADLOG = "[%(threadName)s, %(asctime)s, %(levelname)s] %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=THREADLOG,
                     handlers=[logging.FileHandler("logfile.log")])
 
+
 class ThumbnailMakerService(object):
     def __init__(self, home_dir='.'):
         self.home_dir = home_dir
         self.input_dir = self.home_dir + os.path.sep + 'incoming'
         self.output_dir = self.home_dir + os.path.sep + 'outgoing'
-        self.img_queue = Queue()
-        self.dl_queue = Queue()
+        self.img_list = []
 
-    def download_image(self):
-        while not self.dl_queue.empty():
-            logging.info("attempting download")
+    def download_image(self, dl_queue):
+        logging.info("attempting download")
+        while not dl_queue.empty():
             try:
-                url = self.dl_queue.get(block=False)
-                 # download each image and save to the input dir 
+                url = dl_queue.get(block=False)
+                # download each image and save to the input dir
                 img_filename = urlparse(url).path.split('/')[-1]
                 urlretrieve(url, self.input_dir + os.path.sep + img_filename)
-                self.img_queue.put(img_filename)
-                self.dl_queue.task_done()
+                self.img_list.append(img_filename)
+                dl_queue.task_done()
             except:
                 logging.error("empty queue")
 
-    # def download_images(self, img_url_list):
-    #     # validate inputs
-    #     if not img_url_list:
-    #         return
-    #     os.makedirs(self.input_dir, exist_ok=True)
-        
-    #     logging.info("beginning image downloads")
-
-    #     start = time.perf_counter()
-    #     for url in img_url_list:
-    #         # download each image and save to the input dir 
-    #         img_filename = urlparse(url).path.split('/')[-1]
-    #         urlretrieve(url, self.input_dir + os.path.sep + img_filename)
-    #         self.img_queue.put(img_filename)
-    #     end = time.perf_counter()
-
-    #     self.img_queue.put(None)
-    #     logging.info("downloaded {} images in {} seconds".format(len(img_url_list), end - start))
-
-    def perform_resizing(self):
-        # validate inputs
-        os.makedirs(self.output_dir, exist_ok=True)
-
-        logging.info("beginning image resizing")
+    def resize_img(self, filename):
         target_sizes = [32, 64, 200]
-        num_images = len(os.listdir(self.input_dir))
+        logging.info(f"resizing image multiprocessed {filename}")
+        orig_img = Image.open(self.input_dir + os.path.sep + filename)
+        for basewidth in target_sizes:
+            img = orig_img
+            wpercent = (basewidth / float(img.size[0]))
+            hsize = int((float(img.size[1])*float(wpercent)))
+            img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
+            new_filename = os.path.splitext(
+                filename)[0] + '_' + str(basewidth) + os.path.splitext(filename)[1]
+            img.save(self.output_dir + os.path.sep + new_filename)
 
-        start = time.perf_counter()
-        while True:
-            filename = self.img_queue.get()
-            if not filename:
-                self.img_queue.task_done()
-                return
-            logging.info(f"resizing image {filename}")
-            orig_img = Image.open(self.input_dir + os.path.sep + filename)
-            for basewidth in target_sizes:
-                img = orig_img
-                # calculate target height of the resized image to maintain the aspect ratio
-                wpercent = (basewidth / float(img.size[0]))
-                hsize = int((float(img.size[1]) * float(wpercent)))
-                # perform resizing
-                img = img.resize((basewidth, hsize), PIL.Image.LANCZOS)
-                
-                # save the resized image to the output dir with a modified file name 
-                new_filename = os.path.splitext(filename)[0] + \
-                    '_' + str(basewidth) + os.path.splitext(filename)[1]
-                img.save(self.output_dir + os.path.sep + new_filename)
-
-            os.remove(self.input_dir + os.path.sep + filename)
-            logging.info(f"completed resizing image {filename}")
-            self.img_queue.task_done()
-
-        end = time.perf_counter()
-
-        logging.info("created {} thumbnails in {} seconds".format(num_images, end - start))
+        os.remove(self.input_dir + os.path.sep + filename)
+        logging.info(f"completed resizing image {filename}")
 
     def make_thumbnails(self, img_url_list):
         logging.info("START make_thumbnails")
+        pool = multiprocessing.Pool()
+
         start = time.perf_counter()
 
-        for img in img_url_list:
-            self.dl_queue.put(img)
+        dl_queue = Queue()
 
+        for img in img_url_list:
+            dl_queue.put(img)
+        logging.info("created queue")
         for _ in range(4):
-            t = Thread(target=self.download_image)
+            t = Thread(target=self.download_image, args=(
+                dl_queue,))  # comma creates tuple
             t.start()
 
-        t2 = Thread(target=self.perform_resizing)
-        t2.start()
-        self.dl_queue.join()
-        self.img_queue.put(None) # poison pill
-        t2.join()
-
+        dl_queue.join()
+        pool.map(self.resize_img, self.img_list)
         end = time.perf_counter()
+        pool.close()
+        pool.join()
+
         logging.info("END make_thumbnails in {} seconds".format(end - start))
